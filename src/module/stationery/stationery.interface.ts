7b099ab6bffc147a5433bf71c21c9ea5a082c606
asad406 <asadul76@gmail.com>
2024-11-22 17:56:13 +0600

    stationery interface added

---
+++ src/module/stationery/stationery.interface.ts
@@ -0,0 +1,9 @@
+export interface IStationery {
+    name: string,
+    brand: string,
+    price: number,
+    category: string,
+    description: string,
+    quantity: number,
+    inStock: boolean
+}
